4052c13f1b810256fa8af2bc6efd0a20fe4d7d0c
Luis Mario <itsluismario@gmail.com>
2023-01-27 18:53:18 -0600

    Remove items

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import '@styles/OrderItem.scss';
-import close from '@icons/icon_close.png';
+import iconClose from '@icons/icon_close.png';
 
-const OrderItem = ({ product }) => {
+const OrderItem = ( props ) => {
+	const { product, indexValue } = props;
+	const { removeFromCart } = useContext(AppContext);
+
+	const handleRemove = index => {
+		removeFromCart(index);
+	}
+
 	return (
 		<div className="OrderItem">
 			<figure>
-				<img src={product.images[0]} alt={product.title} />
+				<img 
+					src={product.images[0]} 
+					alt={product.title} 
+				/>
 			</figure>
 			<p>{product.title}</p>
 			<p>${product.price}</p>
-			<img src={close} alt="close" />
+			<img 
+				src={iconClose} 
+				alt="close" 
+				onClick={() => handleRemove(indexValue)} 
+			/>
 		</div>
 	);
 }
